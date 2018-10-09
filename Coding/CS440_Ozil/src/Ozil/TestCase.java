79f19eb2c6b64a752f48a6803601c2bc2a508afd
MorenoAlexander <amoren26@uic.edu>
2018-10-09 11:45:43 -0500

    added skeleton code for TestCase class

--- Coding/CS440_Ozil/src/Ozil/TestCase.java
+++ Coding/CS440_Ozil/src/Ozil/TestCase.java
@@ -1,1 +1,27 @@
 package Ozil;
+
+
+
+
+public Class TestCase implements Serializable {
+ 
+  private String testCaseTitle;
+  private String testCaseDescription;
+  private String authorName;
+  private int authorID;
+  //Date of Create variable
+  //etc...
+  
+  
+  
+  public TestCase(String TestCaseTitle,int authorId,String Name)
+  {
+    testCaseTitle = TestCaseTitle;
+    authorID = authorId;
+    authorName = Name;
+  }
+  
+  
+  
+  
+}
